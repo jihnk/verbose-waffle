30a80223f12911d27932c4a764c213025596e9bb
jihnk <jhyunk218@gmail.com>
2023-01-03 22:29:09 +0900

    feature: add sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
+import Auth from "./pages/Auth";
 import Todo from "./pages/Todo";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/auth" element={<Main />}></Route>
+				<Route path="/auth" element={<Auth />} />
 				<Route path="/todo" element={<Todo />}></Route>
 			</Routes>
 		</BrowserRouter>
---
+++ src/pages/Auth.tsx
@@ -0,0 +1,102 @@
+import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router";
+import { login, signup } from "../api/auth";
+
+const Auth = () => {
+	const navigate = useNavigate();
+
+	const [inputs, setInputs] = useState({
+		email: "",
+		password: "",
+	});
+	const [isValidEmail, setIsValidEmail] = useState(false);
+	const [isValidPassword, setIsValidPassword] = useState(false);
+
+	const emailReg = /[a-z0-9]+@[a-z]+\.[a-z]/;
+	const checkValid = (e: ChangeEvent<HTMLInputElement>) => {
+		switch (e.target.name) {
+			case "email":
+				setIsValidEmail(emailReg.test(e.target.value));
+				break;
+			case "password":
+				setIsValidPassword(e.target.value.length >= 8);
+				break;
+			default:
+				break;
+		}
+	};
+	const [isNew, setIsNew] = useState(false);
+
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value, name } = e.target;
+		checkValid(e);
+		setInputs({
+			...inputs,
+			[name]: value,
+		});
+	};
+
+	const onSubmit = () => {
+		isNew ? getSignUp() : getLogin();
+	};
+
+	const getLogin = async () => {
+		if (localStorage.getItem("token")) {
+			navigate(`/`);
+		}
+		const loginResult = await login(inputs);
+		localStorage.setItem("token", loginResult.token);
+		alert(loginResult.message);
+		navigate(`/`);
+	};
+
+	const getSignUp = async () => {
+		const { data, status } = await signup(inputs);
+		if (status === 200) {
+			alert(data.message);
+			setInputs({ email: "", password: "" });
+			navigate(`/auth`);
+		}
+	};
+
+	const checkIsNew = () => {
+		setIsNew(!isNew);
+	};
+
+	return (
+		<>
+			<h1>회원가입/로그인</h1>
+			<label htmlFor="isNew">
+				회원이 아니라면 체크
+				<input
+					type="checkbox"
+					id="isNew"
+					value="signup"
+					onChange={checkIsNew}
+				/>
+			</label>
+			<form>
+				<input
+					name="email"
+					value={inputs.email}
+					placeholder="이메일"
+					onChange={onChange}
+				/>
+				<input
+					name="password"
+					value={inputs.password}
+					placeholder="비밀번호"
+					onChange={onChange}
+				/>
+				<button
+					onClick={onSubmit}
+					disabled={!isValidEmail || !isValidPassword}
+					type="button"
+				>
+					시작
+				</button>
+			</form>
+		</>
+	);
+};
+export default Auth;
